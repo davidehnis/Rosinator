5c245165c634b5e2addd10b40a58bad15d08c8ec
David Ehnis <david.ehnis@outlook.com>
2019-08-10 08:58:24 -0400

    added nuspec

--- core/Properties/AssemblyInfo.cs
+++ core/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("core")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Rosinator Core Library")]
+[assembly: AssemblyDescription("A simplified approach to the Roslyn APIs")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("core")]
+[assembly: AssemblyCompany("Industrial Digits")]
+[assembly: AssemblyProduct("Rosinator.Core")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
